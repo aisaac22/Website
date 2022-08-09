7ed64ac83d9e683c98ef4d137029701c56a2beb9
aisaac22 <97427884+aisaac22@users.noreply.github.com>
2022-08-08 21:26:37 -0400

    add

---
+++ src/assets/reg.js
@@ -0,0 +1,5 @@
+var c = document.getElementById("myCanvas");
+                var ctx = c.getContext("2d");
+                ctx.font = "30px Times New Roman";
+                ctx.fillStyle = "#f7f0f0";
+                ctx.fillText("Git Hub",52,33);
